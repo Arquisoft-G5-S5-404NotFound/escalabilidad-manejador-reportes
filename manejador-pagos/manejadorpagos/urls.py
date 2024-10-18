99bf53b5807ad04f6cd7c0aecce821b42427d18a
ju4ndiegos <jd.sanchez2@uniandes.edu.co>
2024-10-18 03:33:35 -0500

    healthcheck

--- manejador-pagos/manejadorpagos/urls.py
+++ manejador-pagos/manejadorpagos/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from . import views
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('health/', views.health_check, name='health'),
     path('manejadorpagos/', include('manejadorpagosApp.urls')),
 ]
